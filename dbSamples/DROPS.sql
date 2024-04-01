bd1631179f1c509487b69df18cf00c76a8cee398
1201525GoncaloNogueira <1201525@isep.ipp.pt>
2024-04-01 16:54:15 +0100

    Database backup updated.

--- dbSamples/DROPS.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1710321482829@@127.0.0.1@3306@invoicedb
--- delete the information from the database
-DELETE FROM Regex WHERE nome_empresa = 'Roger & Gallet';
--- delete table from database
-DROP TABLE IF EXISTS Regex;
----```
---- 1710321482829 pc
---- home pc 1709306145245
--- dbSamples/INSERT INTO [dbo].sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO [dbo].[suppliersName] (supplierName, supplierContactName, supplierContactEmail, supplierContactPhone, supplierContactLang, supplierLeavePending, supplierReturnsContactName, supplierReturnsContactEmail, workdaysToDeliver, discountPP, discountAffectIva, cobraIva, daysToPay, contributeToLeadTime, isActive, obsPrivate, obsShared)
-VALUES
-('LEX', 'John Doe', 'johndoe@lex.com', '555-0101', 'EN', 0, 'Jane Doe', 'janedoe@lex.com', 5, 5.0, 0, 1, 30, 1, 1, 'Private notes about LEX', 'Shared notes about LEX'),
-('Roger & Gallet', 'Alexis Duval', 'alexis@rogergallet.com', '555-0202', 'FR', 0, 'Martine Dupont', 'martine@rogergallet.com', 3, 7.0, 1, 1, 45, 1, 1, 'Private notes about Roger & Gallet', 'Shared notes about Roger & Gallet');
