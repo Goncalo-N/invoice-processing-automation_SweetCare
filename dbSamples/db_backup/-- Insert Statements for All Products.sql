697ad3a6a279b166b20d021ea2093ff7d84e3a36
1201525GoncaloNogueira <1201525@isep.ipp.pt>
2024-04-10 16:38:22 +0100

    inserts dbo.supplierOrderItems

---
+++ dbSamples/db_backup/-- Insert Statements for All Products.sql
@@ -0,0 +1,103 @@
+-- Insert Statements for All Products from invoice inv5106641050_LEX_ECOFACTURA_NXO
+
+-- CCREAM ROSTO DUO 40 POUPE8
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '7295378', 'oo', 3, 0, 3, 0, '2024-01-02', 8.26, 21.31, 0, '220052765', 0, 'oo');
+
+-- AK CR NUTRI-REP PES SECOS 50ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6895094', 'oo', 6, 0, 6, 0, '2024-01-02', 5.99, 28.75, 0, '220052765', 0, 'oo');
+
+-- SPRAY MUDA FRALDA 75ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '7076844', 'oo', 3, 0, 3, 0, '2024-01-02', 7.96, 20.54, 0, '220052765', 0, 'oo');
+
+-- AGUA DE LIMPEZA 300 ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6539163', 'oo', 3, 0, 3, 0, '2024-01-02', 5.35, 13.80, 0, '220052765', 0, 'oo');
+
+-- STICK COLD CREAM 10ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6694083', 'oo', 3, 0, 3, 0, '2024-01-02', 5.35, 13.80, 0, '220052765', 0, 'oo');
+
+-- CICASTELA CR REPAR 40ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6282525', 'oo', 90, 0, 90, 0, '2024-01-02', 5.35, 312.97, 0, '220052765', 0, 'oo');
+
+-- AKILDIA CREME PE DIAB 150 ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6937748', 'oo', 3, 0, 3, 0, '2024-01-02', 8.06, 20.79, 0, '220052765', 0, 'oo');
+
+-- SERUM CORRET ESTRIAS 45ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6264432', 'oo', 6, 0, 6, 0, '2024-01-02', 14.03, 72.39, 0, '220052765', 0, 'oo');
+
+-- AKILHIVER CREME FRIERAS 100ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6908368', 'oo', 6, 0, 6, 0, '2024-01-02', 8.33, 34.99, 0, '220052765', 0, 'oo');
+
+-- AGUA DE LIMPEZA 750 ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6887653', 'oo', 3, 0, 3, 0, '2024-01-02', 11.29, 29.13, 0, '220052765', 0, 'oo');
+
+-- EC SERUM REPAR 10 OLEOS 10ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6866046', 'oo', 3, 0, 3, 0, '2024-01-02', 8.10, 20.90, 0, '220052765', 0, 'oo');
+
+-- AKILEINE SPORTS NOK 75ML+OF33%
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6289413', 'oo', 3, 0, 3, 0, '2024-01-02', 6.35, 16.38, 0, '220052765', 0, 'oo');
+
+-- STELATOPIA CR ROSTO 40 DUO
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6397356', 'oo', 12, 0, 12, 0, '2024-01-02', 8.36, 86.28, 0, '220052765', 0, 'oo');
+
+-- AKILHIVER CREME FRIEIRAS 30 ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6841817', 'oo', 3, 0, 3, 0, '2024-01-02', 3.43, 7.20, 0, '220052765', 0, 'oo');
+
+-- PHYSIO SORO FISIOL 5MLX40+OF20
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '7266726', 'oo', 3, 0, 3, 0, '2024-01-02', 5.49, 14.16, 0, '220052765', 0, 'oo');
+
+-- CREME FRALDA 100ML + 50ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6969816', 'oo', 12, 0, 12, 0, '2024-01-02', 5.93, 61.20, 0, '220052765', 0, 'oo');
+
+-- LINIMENTO HIG. FRALDA 400ML
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6937755', 'oo', 24, 0, 24, 0, '2024-01-02', 8.32, 171.72, 0, '220052765', 0, 'oo');
