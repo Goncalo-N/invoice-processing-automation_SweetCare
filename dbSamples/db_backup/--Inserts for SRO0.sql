3cef7f3dabfaba3e854c06155a99cb575263e176
1201525GoncaloNogueira <1201525@isep.ipp.pt>
2024-04-24 15:08:03 +0100

    inserts for local testing validation

---
+++ dbSamples/db_backup/--Inserts for SRO0.sql
@@ -0,0 +1,17 @@
+-- JMF SABONETES REDONDOS 3x100G
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6861740', 'oo', 3, 0, 3, 0, '2024-01-13', 10.90, 24.54, 0, 'SRO0412FAC02596', 0, 'JMF Sabonetes Redondos');
+
+-- CEDRAT SABONETE 100g
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6862102', 'oo', 3, 0, 3, 0, '2024-01-13', 4.20, 9.45, 0, 'SRO0412FAC02596', 0, 'Cedrat Sabonete');
+
+-- ROSE KIT SABONETES 3X100g
+INSERT INTO [sweet].[dbo].[supplierOrderItems]
+([orderId], [ref], [subType], [qntOrder], [qntOrderBonus], [qntDelivery], [qntDeliveryBonus], [deliveryDate], [priceNoBonus], [priceWithBonus], [isStockUpdated], [supplierInvoiceNumber], [isFactUpdated], [obs])
+VALUES
+(2, '6861823', 'oo', 3, 0, 3, 0, '2024-01-13', 10.90, 24.54, 0, 'SRO0412FAC02596', 0, 'Rose Kit Sabonetes');
